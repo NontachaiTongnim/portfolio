09a1452b1682d152e3b6000a73e647c3af5dbe3a
NontachaiTongnim <nontachai.tongnim@gmail.com>
2024-03-28 22:30:49 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/portfolio.nontachai/',
+  base:'/portfolio/',
   plugins: [vue()],
   server:{
     port: 4200
